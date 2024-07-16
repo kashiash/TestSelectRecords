2cc599df750f7885301ecc7479a39be6787c5976
Jacek Kosiński <jacek.kosinski@uta.com>
2024-07-16 22:15:43 +0200

    userdc

---
+++ TestSelectRecords.Module/BusinessObjects/UsersDC.cs
@@ -0,0 +1,14 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using System;
+using System.Linq;
+
+namespace TestSelectRecords.Module.BusinessObjects
+{
+    [DomainComponent]
+    public class UsersDC : NonPersistentLiteObject
+    {
+
+        public string Name { get; set; }
+    }
+}
